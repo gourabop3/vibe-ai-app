fe2edf0b00005555b61f31dfbe0d911d36c1ce0c
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-07-08 00:03:30 +0100

    17: billing

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -3,6 +3,7 @@ import { TRPCError } from "@trpc/server";
 
 import { prisma } from "@/lib/db";
 import { inngest } from "@/inngest/client";
+import { consumeCredits } from "@/lib/usage";
 import { protectedProcedure, createTRPCRouter } from "@/trpc/init";
 
 export const messagesRouter = createTRPCRouter({
@@ -49,41 +50,53 @@ export const messagesRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ input, ctx }) => {
-      try {
-        const existingProject = await prisma.project.findUnique({
-          where: {
-            id: input.projectId,
-            userId: ctx.auth.userId,
-          },
-        });
+      const existingProject = await prisma.project.findUnique({
+        where: {
+          id: input.projectId,
+          userId: ctx.auth.userId,
+        },
+      });
 
-        if (!existingProject) {
+      if (!existingProject) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Project not found",
+        });
+      }
+
+      try {
+        await consumeCredits();
+      } catch (error) {
+        if (error instanceof Error) {
           throw new TRPCError({
-            code: "NOT_FOUND",
-            message: "Project not found",
+            code: "BAD_REQUEST",
+            message: "Something went wrong",
+          });
+        } else {
+          throw new TRPCError({
+            code: "TOO_MANY_REQUESTS",
+            message: "You have run out of credits",
           });
         }
-
-        const createdMessage = await prisma.message.create({
-          data: {
-            projectId: existingProject.id,
-            content: input.value,
-            role: "USER",
-            type: "RESULT",
-          },
-        });
-
-        await inngest.send({
-          name: "code-agent/run",
-          data: {
-            value: input.value,
-            projectId: input.projectId,
-          },
-        });
-
-        return createdMessage;
-      } catch (error) {
-        console.error(error);
       }
+
+      const createdMessage = await prisma.message.create({
+        data: {
+          projectId: existingProject.id,
+          content: input.value,
+          role: "USER",
+          type: "RESULT",
+        },
+      });
+
+      await inngest.send({
+        name: "code-agent/run",
+        data: {
+          value: input.value,
+          projectId: input.projectId,
+        },
+      });
+
+      return createdMessage;
     }),
 });
